acc9ff82da968ec743f3044f57bc3acb58245159
hankail05 <39765074+hankail05@users.noreply.github.com>
2019-10-26 13:16:27 +0900

    resolve queueing not converted RJ code bug

--- src/converter.py
+++ src/converter.py
@@ -21,17 +21,21 @@ def rj_convert(directory: str):
 
 def rj_convert_recursive(directory: str):
     """Convert like the above function bu recursively."""
-    subdirs = [directory + x + '/' for x in os.listdir(directory) if os.path.isdir(directory + x)]
+    subdirs = [directory + x + '/'
+               for x in os.listdir(directory)
+               if os.path.isdir(directory + x) and x[0] != '.']
     rj_regex = re.compile("(RJ|rj)[0-9]{6}")
     queue = []
     for subdir in subdirs:
         regex_result = rj_regex.search(subdir)
         if regex_result is not None:
             rj = re.sub(r"rj", "RJ", regex_result.group())
-            name = dl.fetch_work(rj).name
+            name = directory + dl.fetch_work(rj).name
             print("{} -> {}".format(subdir, name))
             os.rename(subdir, os.path.join(directory, name))
-        queue.append(subdir)
+            queue.append(name)
+        else:
+            queue.append(subdir)
     while queue:
         subdir = queue.pop(0)
         print("Entering directory {}".format(subdir))
@@ -48,7 +52,6 @@ if __name__ == "__main__":
                         help="to convert name of its subdirectories")
     args = parser.parse_args()
     if args.recursive:
-        print(os.path.abspath(args.directory) + '/')
         rj_convert_recursive(os.path.abspath(args.directory) + '/')
     else:
         rj_convert(os.path.abspath(args.directory) + '/')
